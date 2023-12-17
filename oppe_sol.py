dd99d3de7c8ce0659a44f31654fd90f610b4db3f
srupat <srujanpatwardhan@gmail.com>
2023-12-17 11:15:17 +0530

    peak unimodal

--- oppe_sol.py
+++ oppe_sol.py
@@ -15,4 +15,23 @@ def find_first_occurence(A, k):
 
     return -1
 
-print(find_first_occurence([1,1,1,1,2,2,2,3,3,3,3,3,4,4], 2))
+def peak_unimodal(A):
+    low = 0
+    high = len(A) - 1
+    while high>= low:
+        mid = (high + low) // 2
+
+        if A[mid-1] < A[mid] > A[mid + 1]:
+            return mid
+        
+        if A[mid-1] < A[mid]:
+            low = mid + 1
+
+        else:
+            high = mid -1
+
+    return -1
+
+# print(find_first_occurence([1,1,1,1,2,2,2,3,3,3,3,3,4,4], 2))
+
+print(peak_unimodal([1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,0]))

225d5e4c74dc83091b820e7736a6f5854cbff4c7
srupat <srujanpatwardhan@gmail.com>
2023-12-17 11:06:09 +0530

    oppe qs that I couldnt do

---
+++ oppe_sol.py
@@ -0,0 +1,18 @@
+def find_first_occurence(A, k):
+    low = 0
+    high = len(A) - 1
+    while high >= low:
+        mid = (high + low) // 2
+
+        if A[mid-1] < A[mid] and A[mid] == k:
+            return mid
+        
+        if A[mid] < k:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+print(find_first_occurence([1,1,1,1,2,2,2,3,3,3,3,3,4,4], 2))
